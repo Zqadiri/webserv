f6d4c27621e9e279998557a9e185933de4cc004d
Zqadiri <zinebghost@gmail.com>
2022-05-19 16:03:00 +0100

    clean

--- src/CGI/CGI.cpp
+++ src/CGI/CGI.cpp
@@ -6,7 +6,7 @@
 /*   By: zqadiri <zqadiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 14:08:22 by nwakour           #+#    #+#             */
-/*   Updated: 2022/05/19 15:34:25 by zqadiri          ###   ########.fr       */
+/*   Updated: 2022/05/19 16:02:55 by zqadiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,6 @@
 //! to_string c++11
 
 /*--------- Constructors & Destructor --------*/
-
-// void		CgiHandler::_initEnv(Request &request, RequestConfig &config) {
-// 	std::map<std::string, std::string>	headers = request.getHeaders();
-// 	if (headers.find("Auth-Scheme") != headers.end() && headers["Auth-Scheme"] != "")
-// 		this->_env["AUTH_TYPE"] = headers["Authorization"];
-
-// 	this->_env["REDIRECT_STATUS"] = "200"; //Security needed to execute php-cgi
-// 	this->_env["GATEWAY_INTERFACE"] = "CGI/1.1";
-// 	this->_env["SCRIPT_NAME"] = config.getPath();
-// 	this->_env["SCRIPT_FILENAME"] = config.getPath();
-// 	this->_env["REQUEST_METHOD"] = request.getMethod();
-// 	this->_env["CONTENT_LENGTH"] = to_string(this->_body.length());
-// 	this->_env["CONTENT_TYPE"] = headers["Content-Type"];
-// 	this->_env["PATH_INFO"] = request.getPath(); //might need some change, using config path/contentLocation
-// 	this->_env["PATH_TRANSLATED"] = request.getPath(); //might need some change, using config path/contentLocation
-// 	this->_env["QUERY_STRING"] = request.getQuery();
-// 	this->_env["REMOTEaddr"] = to_string(config.getHostPort().host);
-// 	this->_env["REMOTE_IDENT"] = headers["Authorization"];
-// 	this->_env["REMOTE_USER"] = headers["Authorization"];
-// 	this->_env["REQUEST_URI"] = request.getPath() + request.getQuery();
-// 	if (headers.find("Hostname") != headers.end())
-// 		this->_env["SERVER_NAME"] = headers["Hostname"];
-// 	else
-// 		this->_env["SERVER_NAME"] = this->_env["REMOTEaddr"];
-// 	this->_env["SERVER_PORT"] = to_string(config.getHostPort().port);
-// 	this->_env["SERVER_PROTOCOL"] = "HTTP/1.1";
-// 	this->_env["SERVER_SOFTWARE"] = "Weebserv/1.0";
-
-// 	this->_env.insert(config.getCgiParam().begin(), config.getCgiParam().end());
-// }
 
 CGI::CGI(const request &request, const serverConfig &server)
 {
@@ -95,7 +65,6 @@ CGI	&CGI::operator=(CGI const &src) {
 
 std::string		CGI::executeCgi(const std::string& scriptName, int socket_fd)
 {
-	(void)server;
 	std::string output;
 	pid_t		pid;
 	int			savedIn;
