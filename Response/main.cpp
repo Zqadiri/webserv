4fcf895d01250e77d42586ef14265acfd32ec323
Mohammed ali Kellati <mokellat@e1r5p2.1337.ma>
2022-05-22 01:21:43 +0100

    cgi have to be reimplemented

--- Response/main.cpp
+++ Response/main.cpp
@@ -1,22 +1,43 @@
 #include "Response.hpp"
 #include "MimeTypes.hpp"
+#include <sys/stat.h>
 
-void File_type(std::string req)
+
+// void File_type(std::string req)
+// {
+
+//     std::string str;
+//     std::string str2;
+//     int         index;
+
+//     str = req;
+//     index = str.find_first_of(".");
+//     str2 = str.substr(index+1, str.length());
+//     std::cout << str2 << std::endl;
+//     // return ext;
+// }
+
+int File_lenght(std::string req)
 {
+    // we gonna calculate the length of our file (body lenght)
+    int             ret;
+    std::fstream    myfile(req);
+    struct          stat sb;
 
-    std::string str;
-    std::string str2;
-    int         index;
-
-    str = req;
-    index = str.find_first_of(".");
-    str2 = str.substr(index+1, str.length());
-    std::cout << str2 << std::endl;
-    // return ext;
+    if(!stat(req.c_str(), &sb))
+    {
+        ret = sb.st_size;
+        puts("1");
+    }
+    else
+        ret = -1;
+    return ret;
 }
+
 int main()
 {
+    int str;
     // Response res;
-    File_type("index.php");
-    // std::cout << File_type("index.php") << std::endl;
+    str = File_lenght("./response_errors_pages/400.html");
+    std::cout << str << std::endl;
 }
